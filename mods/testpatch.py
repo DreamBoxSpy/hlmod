cc1dfa93a978835e0c085d161ac620c6b5719671
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2025-08-28 20:57:58 -0500

    chore: bump hlmod-hl, type testpatch

--- mods/testpatch.py
+++ mods/testpatch.py
@@ -15,7 +15,12 @@ def initialize():
     assert_code_sha("c5b81c94db4a9de0e78e8779adabddf6b4246fd1fc938307306c27271e2df826")
 
 @hook(30)
-def thing(self: Hook, *args):
+def thing(self: Hook, val: float, val2: Optional[float], msg: str, val3: Optional[TestClass]):
     print("Hook!")
-    self.call_original(*args)
+    val = 2.0
+    val2 = 1.0
+    msg = "Hello, hlmod world!"
+    assert val3 is not None
+    print(val3.test)
+    self.call_original(val, val2, msg, val3)
 
