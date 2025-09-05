718517620be1f2b9f2630269068794c6ea7c79d9
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2025-09-05 12:57:07 -0500

    feat: hlsteam support

--- mods/testpatch.py
+++ mods/testpatch.py
@@ -12,15 +12,19 @@ from stubs import TestClass
 from typing import Optional
 
 def initialize():
-    assert_code_sha("c5b81c94db4a9de0e78e8779adabddf6b4246fd1fc938307306c27271e2df826")
+    # Haxe 4.3.6 Windows
+    # assert_code_sha("c5b81c94db4a9de0e78e8779adabddf6b4246fd1fc938307306c27271e2df826")
+    # haxe 4.3.6 Linux
+    # assert_code_sha("52ac527751d7aa20d1be9024de88628f389336dbed3d915d82f55cabf58c3617")
+    pass
 
-@hook(30)
-def thing(self: Hook, val: float, val2: Optional[float], msg: str, val3: Optional[TestClass]):
-    print("Hook!")
-    val = 2.0
-    val2 = 1.0
-    msg = "Hello, hlmod world!"
-    assert val3 is not None
-    print(val3.test)
-    self.call_original(val, val2, msg, val3)
+# @hook(30)
+# def thing(self: Hook, val: float, val2: Optional[float], msg: str, val3: Optional[TestClass]):
+#     print("Hook!")
+#     val = 2.0
+#     val2 = 1.0
+#     msg = "Hello, hlmod world!"
+#     assert val3 is not None
+#     print(val3.test)
+#     self.call_original(val, val2, msg, val3)
 
